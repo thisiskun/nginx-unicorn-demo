aaa01a26064e458cd5f6e7005e66521ec5cef7ad
thisiskun <chenchaokun@gmail.com>
2013-03-13 10:35:39 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NginxUnicornDemo::Application.config.secret_token = 'e045c187627893282287e120176c144894fc290cd5b1fa911d11db2540c8dec2d29ef54a64a90b85d034437cfd152d667b3ced4914bb8fb30bf952123ed7ecc7'
